6f4b6b5818e87862dc1cfd549fdceaddc77cc4bb
JesseTzh <jessetzh@gmail.com>
2020-05-10 14:57:41 +0800

    实现代码与Github直接同步

--- config.creep.js
+++ config.creep.js
@@ -11,23 +11,25 @@ module.exports = {
      *   矿工配置文件，需传入一个指定Energy ID
      */
     Harvester_01: harvester(CONFIG.ENERGY_SOURCE[0]),
-    Harvester_02: harvester(CONFIG.ENERGY_SOURCE[1]),
+    Harvester_02: harvester(CONFIG.ENERGY_SOURCE[0]),
     Harvester_03: harvester(CONFIG.ENERGY_SOURCE[0]),
     Harvester_04: harvester(CONFIG.ENERGY_SOURCE[1]),
     Harvester_05: harvester(CONFIG.ENERGY_SOURCE[1]),
+    Harvester_06: harvester(CONFIG.ENERGY_SOURCE[1]),
 
     /**
      *   升级工配置文件，默认需要传入一个距离 Controller 较近的能量存储设备，Link为最佳
      */
     Upgrader_01: upgrader(CONFIG.UPGRADE_ENERGY_SOURCE[0]),
-    Upgrader_02: upgrader(CONFIG.UPGRADE_ENERGY_SOURCE[0]),
+    //Upgrader_02: upgrader(CONFIG.UPGRADE_ENERGY_SOURCE[0]),
     Upgrader_03: upgrader(CONFIG.UPGRADE_ENERGY_SOURCE[0]),
+    Upgrader_04: upgrader(CONFIG.UPGRADE_ENERGY_SOURCE[0]),
 
     /**
      *   建筑工配置文件，默认需要传入一个冗余能量存储设备作为建造资源提取处，config文件中可配置是否允许进一步从其他建筑提取能量
      */
     Builder_01: builder(CONFIG.UPGRADE_ENERGY_SOURCE[0]),
-    //171,465
+    //Builder_02: builder(CONFIG.UPGRADE_ENERGY_SOURCE[0]),
 
     /**
      *   搬运工配置文件，默认需要传入一个不允许从中提取能量的储能建筑ID，一般为Link接收端
--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 module.exports = {
     //能量矿点 ID
-    ENERGY_SOURCE: ['5bbcabd89099fc012e6345d7','5bbcabd89099fc012e6345d9'],
+    ENERGY_SOURCE: ['5bbcad489099fc012e63708d','5bbcad489099fc012e63708f'],
 
     //Tower ID
-    //TOWER: ['9bdd6bbbd928bee','527b891ac6ed7c1'],
+    TOWER: ['5eb7147d7f3603f6f5b2f2ec'],
 
     //Link ID,二维数组形式储存，LINK_SEND[n][0]为发射端，LINK_SEND[n][1]为接收端
     //LINK: [['5c217f9ee48c223','58967f82539dd78'],['3c549898f9c6b76','58967f82539dd78']],
 
     //升级 Controller 所用的能量来源
-    UPGRADE_ENERGY_SOURCE: ['5eb4131a7b6fd7d2b27d297c'],
+    UPGRADE_ENERGY_SOURCE: ['5eb569554dac05ff668051db'],
 
     //Storage ID,一般用作冗余能量缓冲池与建筑建造能量来源
     //STORAGE: ['e875d5c3fdb26da']
