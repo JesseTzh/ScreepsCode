e9c6783738cdbf401e78142fcffa44118ceda16d
JesseTzh <jessetzh@gmail.com>
2020-05-12 14:28:07 +0800

    优化Upgrader能量获取逻辑

--- config.system.setting.js
+++ config.system.setting.js
@@ -3,6 +3,8 @@ module.exports = {
     ALLOW_BUILDE_FROM_SE: true,
     //是否允许搬运工将多余能量存储进 STORAGE/CONTAINER （如有 STORAGE 请谨慎打开）
     ALLOW_MOVER_STORAGE: false,
+    //是否允许升级工从 EXTENSION/SPAWN 中提取能量，前期不建议打开，容易使Upgrader过度提取能量
+    ALLOW_UPGRADER_FROM_SE: false,
     //是否按照已铺好Road的情况生成Creep
     ROAD_FLAG: true,
     //允许制造Creep的最大能量消耗值
@@ -13,7 +15,7 @@ module.exports = {
     LOGGER_LEVEL: "INFO",
     //是否打开GUI调试
     GUIDEBUGMODE: false,
-    //房间可用能量高于多少时允许向Upgrader分配能量
+    //房间可用能量高于多少时允许将Link中的能量传输给Upgrader
     ALLOW_UPGRADER_USE_ENERGY: 0.3,
     //能量报警界限
     ENERGY_ALERT_NUM: 0.5,
--- new_role.upgrader.js
+++ new_role.upgrader.js
@@ -10,13 +10,24 @@ module.exports = sourceId => ({
             source = null;
         }
         if (source == null) {
-            logger.info("尝试从其他建筑获取");
+            //默认取能建筑为空，尝试从其他冗余储能建筑提取能量
+            logger.info(creep.name + "尝试从冗余建筑获取");
             source = creep.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_LINK || structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                    return (structure.structureType == STRUCTURE_STORAGE || structure.structureType == STRUCTURE_LINK) &&
                         structure.store[RESOURCE_ENERGY] > 0;
                 }
             });
+            //冗余储能建筑也为空，若在配置文件中允许，则从 EXTENSION/SPAWN 提取能量
+            if (source == null && SYS_CONFIG.ALLOW_UPGRADER_FROM_SE) {
+                logger.info(creep.name + "尝试从 EXTENSION/SPAWN 获取");
+                source = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.store[RESOURCE_ENERGY] > 0;
+                    }
+                });
+            }
         }
         if (source) {
             if (creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
