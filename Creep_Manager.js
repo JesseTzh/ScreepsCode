2509cf4b0f326d122f41ad41e50a9f28eefd887c
JesseTzh <jessetzh@gmail.com>
2020-05-22 14:38:09 +0800

    增加复活失败邮件提醒

--- Creep_Manager.js
+++ Creep_Manager.js
@@ -5,7 +5,7 @@ const creepTemplateConfigs = require('config.creep.template')
 function creepManager() {
     for (name in creepConfigs) {
         if (!Game.creeps[name]) {
-            if(Memory.creeps[name] && Memory.creeps[name].RebornFlag && Memory.creeps[name].RebornFlag == "No"){
+            if (Memory.creeps[name] && Memory.creeps[name].RebornFlag && Memory.creeps[name].RebornFlag == "No") {
                 logger.info('取消重生：' + name);
                 continue;
             }
@@ -22,6 +22,9 @@ function creepManager() {
                     logger.warn(name + " 重生失败！错误代码：" + result)
                 } else if (result == ERR_NOT_ENOUGH_ENERGY) {
                     Memory.creeps[name].RebornFailTimes ? Memory.creeps[name].RebornFailTimes += 1 : Memory.creeps[name].RebornFailTimes = 1;
+                    if (Memory.creeps[name].RebornFailTimes > 1000) {
+                        Game.notify('Creep ' + name + '长达1000ticks复活失败！');
+                    }
                 } else {
                     logger.info('正在重生 : ' + name);
                     //删除之前Creep记忆
