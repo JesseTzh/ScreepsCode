6d48aa208a98834841ef2068f5cb1c60d0cbb2bf
JesseTzh <jessetzh@gmail.com>
2020-06-01 16:14:50 +0800

    增加判断对象类型方法

--- utils.tools.js
+++ utils.tools.js
@@ -56,6 +56,14 @@ function detectRoomInvaderCore() {
     }
 }
 
+function getType(obj) {
+    var type = Object.prototype.toString.call(obj).match(/^\[object (.*)\]$/)[1].toLowerCase();
+    if(type === 'string' && typeof obj === 'object') return 'object'; // Let "new String('')" return 'object'
+    if (obj === null) return 'null'; // PhantomJS has type "DOMWindow" for null
+    if (obj === undefined) return 'undefined'; // PhantomJS has type "DOMWindow" for undefined
+    return type;
+}
+
 module.exports = {
     globalEnergyMonitor: globalEnergyMonitor,
     energySourceMonitor: energySourceMonitor,
