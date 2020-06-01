7245af1f53e8caee4c9af7e7d5fa92dfb1912255
JesseTzh <jessetzh@gmail.com>
2020-06-01 17:27:20 +0800

    修改为远程拆迁工

--- Role_Dismantler.js
+++ Role_Dismantler.js
@@ -2,31 +2,30 @@ const logger = require('utils.log').getLogger("Role_Builder");
 const SYS_CONFIG = require('config.system.setting');
 
 module.exports = config => ({
-    // 拆迁
+    // 前往目标房间
     source: creep => {
-        var source = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType === STRUCTURE_TOWER || structure.structureType === STRUCTURE_EXTENSION || structure.structureType === STRUCTURE_LINK || structure.structureType === STRUCTURE_STORAGE) &&
-                    structure.store[RESOURCE_ENERGY] > 0;
-            }
-        });
-        if(source) {
-            if(creep.withdraw(source) === ERR_NOT_IN_RANGE) {
-                creep.say("🔽")
-                creep.moveTo(source);
-            }
-        }
+        //creep.moveTo(new RoomPosition(config.pathFinderPoint[0][0], config.pathFinderPoint[0][1], config.targetRoom))
     },
-    // 建造
+    // 拆迁
     target: creep => {
-        var target = Game.getObjectById(config.targetId)
-        if (target && target.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
-            if (creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                creep.say("🔼");
+        const target = Game.getObjectById(config.targetId);
+        if (target) {
+            if (creep.dismantle(target) === ERR_NOT_IN_RANGE) {
+                creep.say("80!80!");
                 creep.moveTo(target);
             }
         }
     },
     // 状态切换条件
-    switch: creep => creep.updateState()
+    switch: creep => {
+        // creep 没有抵达目标房间 && creep 之前的状态为“工作”
+        if (creep.room.name !== creep.memory.TargetRoom && creep.memory.working) {
+            creep.memory.working = false
+        }
+        // creep 抵达目标房间 && creep 之前的状态为“不工作”
+        if ((creep.room.name === creep.memory.TargetRoom && !creep.memory.working)) {
+            creep.memory.working = true
+        }
+        return creep.memory.working
+    }
 })
