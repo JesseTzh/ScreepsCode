a3aefd9b035a910866f3e037f3b5fa353682706b
JesseTzh <jessetzh@gmail.com>
2020-06-30 15:18:59 +0800

    增加外矿信息配置文件

---
+++ config.outerworld.js
@@ -0,0 +1,11 @@
+// 外矿 id 与其对应 Container
+module.exports = {
+    // [n][0]为矿物 id ,[n][1]为 Container id
+    E5S21: [['5bbcad3a9099fc012e636e4b', '5ef8326d892531028bb766f6'], ['5bbcad3a9099fc012e636e49', '']],
+    E5S22: [['5bbcad3a9099fc012e636e4e', '']],
+    E6S23: [['5bbcad489099fc012e637092', '']],
+    E9S22: [['5bbcad7a9099fc012e637578', ''], ['5bbcad7a9099fc012e63757a', '']],
+    E9S23: [['5bbcad7b9099fc012e63757d', '5ef83746c1b8855715d312f6'], ['5bbcad7b9099fc012e63757e', '5ef8412473c213159115edc6']],
+    E8S24: [['5bbcad6c9099fc012e6373f3', '5ef94d37323f2fbb2458472d']],
+    E7S23: [['5bbcad579099fc012e637262', '5efae5edf67ab850ffd08de7']]
+}
