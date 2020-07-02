668958c626c18b8f5ba01d772d53ad33461ae57b
JesseTzh <jessetzh@gmail.com>
2020-07-02 14:04:16 +0800

    增加注释

--- config.construction.factory.js
+++ config.construction.factory.js
@@ -1,13 +1,24 @@
+/**
+ *   工厂工作配置文件
+ *      参数：
+ *          WorkFlag: 工厂是否工作
+*           Production: 工厂生产产品
+*           Interval: 工厂工作冷却时间
+ *
+ */
 module.exports = {
     E6S22:({
+        WorkFlag: false,
         Production: RESOURCE_ZYNTHIUM_BAR,
         Interval: 21
     }),
     E9S21:({
-        Production: RESOURCE_ORGANOID,
+        WorkFlag: true,
+        Production: RESOURCE_OXIDANT,
         Interval: 21
     }),
     E8S23:({
+        WorkFlag: false,
         Production: RESOURCE_REDUCTANT,
         Interval: 21
     })
