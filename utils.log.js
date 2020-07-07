578cc315d50261934a580bb991dece74b3efc995
JesseTzh <jessetzh@gmail.com>
2020-07-07 10:11:39 +0800

    格式化

--- utils.log.js
+++ utils.log.js
@@ -12,62 +12,62 @@
  */
 const SYS_CONFIG = require('config.system.setting')
 
-class Logger{
-    constructor(){
-        this.LEVEL = new Map([["DEBUG",1],["INFO",2],["WARN",3],["ERROR",4],["OFF",5]])
+class Logger {
+    constructor() {
+        this.LEVEL = new Map([["DEBUG", 1], ["INFO", 2], ["WARN", 3], ["ERROR", 4], ["OFF", 5]])
         this.logSettingNum = 5;
-        this.forceControl=null;
+        this.forceControl = null;
         this.name = "";
     }
 
-    static getLogger(name){
+    static getLogger(name) {
         let logger = new Logger();
         logger.name = name;
         let num = logger.LEVEL.get(SYS_CONFIG.LOGGER_LEVEL);
-        num ? logger.logSettingNum=num:logger.logSettingNum=5;
+        num ? logger.logSettingNum = num : logger.logSettingNum = 5;
         return logger;
     }
 
-    _canLog(levelNum){
-        if(levelNum==null) return false;
-        return this.forceControl==true||levelNum>=this.logSettingNum&&this.forceControl==null
+    _canLog(levelNum) {
+        if (levelNum == null) return false;
+        return this.forceControl == true || levelNum >= this.logSettingNum && this.forceControl == null
     }
 
-    _log(level,messages){
-        if(this._canLog(this.LEVEL.get(level.trim()))){
-            console.log("["+level+"]["+this.name+"]",messages.join(" "));
+    _log(level, messages) {
+        if (this._canLog(this.LEVEL.get(level.trim()))) {
+            console.log("[" + level + "][" + this.name + "]", messages.join(" "));
         }
     }
 
-    off(){
-        this.forceControl=false;
+    off() {
+        this.forceControl = false;
         return this;
     }
 
-    on(){
-        this.forceControl=true;
+    on() {
+        this.forceControl = true;
         return this;
     }
 
-    reset(){
-        this.forceControl=null;
+    reset() {
+        this.forceControl = null;
         return this;
     }
 
-    debug(...message){
-        this._log("DEBUG",message)
+    debug(...message) {
+        this._log("DEBUG", message)
     }
 
-    info(...message){
-        this._log("INFO ",message)
-    }
-    
-    warn(...message){
-        this._log("WARN ",message)
+    info(...message) {
+        this._log("INFO ", message)
     }
 
-    error(...message){
-        this._log("ERROR",message)
+    warn(...message) {
+        this._log("WARN ", message)
+    }
+
+    error(...message) {
+        this._log("ERROR", message)
     }
 }
 
