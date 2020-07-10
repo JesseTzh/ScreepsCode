19c70ea33607364cd572fc2d93b6d9cfe49b8f04
JesseTzh <jessetzh@gmail.com>
2020-07-10 15:04:49 +0800

    无参化矿工

--- Role_Miner.js
+++ Role_Miner.js
@@ -1,9 +1,9 @@
 const logger = require('utils.log').getLogger("Miner");
 
-module.exports = config => ({
+module.exports = ({
     // 采集矿物
     source: creep => {
-        const source = Game.getObjectById(config.sourceId)
+        const source = Game.getObjectById(creep.room.getMineral())
         if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
             creep.say("⛏");
             creep.moveTo(source);
@@ -12,13 +12,9 @@ module.exports = config => ({
     // 存储矿物逻辑
     target: creep => {
         let target;
-        if (config.targetId) {
-            target = Game.getObjectById(config.targetId);
-        } else {
-            target = creep.room.storage;
-        }
+        target = creep.room.storage;
         if (target) {
-            if (creep.transfer(target, Game.getObjectById(config.sourceId).mineralType) === ERR_NOT_IN_RANGE) {
+            if (creep.transfer(target, Game.getObjectById(creep.room.getMineral()).mineralType) === ERR_NOT_IN_RANGE) {
                 creep.say("🔼");
                 creep.moveTo(target);
             }
@@ -28,12 +24,12 @@ module.exports = config => ({
     },
     // 状态切换条件 
     switch: creep => {
-        // creep 身上没有矿物 && creep 之前的状态为“工作”
-        if (creep.store[Game.getObjectById(config.sourceId).mineralType] === 0 && creep.memory.working) {
+        //creep 身上没有矿物 && creep 之前的状态为“工作”
+        if (creep.store[Game.getObjectById(creep.room.getMineral()).mineralType] === 0 && creep.memory.working) {
             creep.memory.working = false
         }
-        // creep 身上矿物满了 && creep 之前的状态为“不工作”
-        if (creep.store[Game.getObjectById(config.sourceId).mineralType] === creep.store.getCapacity() && !creep.memory.working) {
+        //creep 身上矿物满了 && creep 之前的状态为“不工作”
+        if (creep.store[Game.getObjectById(creep.room.getMineral()).mineralType] === creep.store.getCapacity() && !creep.memory.working) {
             creep.memory.working = true
         }
         return creep.memory.working
