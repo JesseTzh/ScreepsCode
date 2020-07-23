0443b01611612816d46c362586a0d704649d79d4
JesseTzh <jessetzh@gmail.com>
2020-07-23 11:21:57 +0800

    初步尝试初始化Creep数据库

--- Database_Initialization.js
+++ Database_Initialization.js
@@ -1,9 +1,37 @@
 const logger = require('utils.log').getLogger("Database");
 const CONFIG = require('config');
 const RoomData = require('Room_Data');
+const BASIC_CREEP = require('Database_Creep_Basic');
+const ADVANCED_CREEP = require('Database_Creep_Advanced');
+const tool = require('utils.tools');
 
 function init() {
-    var roomDataMap = new Map();
+    //房间数据初始化
+    roomDataInit();
+    //Creep数据初始化
+    creepDataInit();
+}
+
+function creepDataInit() {
+    let creepDataMap = new Map();
+    for (let roomName of CONFIG.CLAIM_ROOM) {
+        let creepArray = [];
+        //基础Creep遍历
+        for (let creepName in BASIC_CREEP[roomName]) {
+            creepArray.push(creepName);
+        }
+        //进阶Creep遍历
+        for (let creepName in ADVANCED_CREEP[roomName]) {
+            creepArray.push(creepName);
+        }
+        //将基础与进阶Creep存入同一个Map值中
+        creepDataMap.set(roomName, creepArray);
+    }
+    global.creepData = creepDataMap;
+}
+
+function roomDataInit() {
+    let roomDataMap = new Map();
     for (let claimRoomName of CONFIG.CLAIM_ROOM) {
         let roomData = new RoomData().initData(claimRoomName);
         if (roomData) {
@@ -13,10 +41,6 @@ function init() {
         }
     }
     global.roomData = roomDataMap;
-}
-
-function creepDataInit(){
-
 }
 
 module.exports = {
